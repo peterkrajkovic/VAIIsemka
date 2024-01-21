705cdcb6e5fa82b0bc7388a37d84141d024c1d43
Peter Krajkovič <128690097+peterkrajkovic@users.noreply.github.com>
2024-01-21 10:24:28 +0100

    mails

---
+++ ServerSide/Mails/General.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using sib_api_v3_sdk.Api;
+using sib_api_v3_sdk.Client;
+using sib_api_v3_sdk.Model;
+using System.Diagnostics;
+using System.Text;
+
+namespace ServerApi.Mails
+{
+    public class General
+    {
+        static string apiKey;
+        private static General instance;
+        private General()
+        {
+            var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("Mails/appsettings.Mails.json")
+                    .Build();
+
+            // Retrieve the connection string
+            apiKey = (string)configuration.GetConnectionString("DefaultConnection")!.Trim();
+        }
+
+        // Public method to access the singleton instance
+        public static General GetInstance
+        {
+            get
+            {
+                // Double-check locking for thread safety
+                if (instance == null)
+                {
+                    lock (new object())
+                    {
+                        if (instance == null)
+                        {
+                            instance = new General();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        internal async Task<string> Send(string recipient, string htmlBody, string subject)
+        {
+            Configuration.Default.AddApiKey("api-key", apiKey);
+            var apiInstance = new TransactionalEmailsApi();
+            var sendSmtpEmail = new SendSmtpEmail(new SendSmtpEmailSender("Spark", "spark@europe.com"), new List<SendSmtpEmailTo> { new SendSmtpEmailTo(recipient) },null,null,htmlBody,null,"Verification"); // SendSmtpEmail | Values to send a transactional email
+
+            // Send a transactional email
+            CreateSmtpEmail result = apiInstance.SendTransacEmail(sendSmtpEmail);
+            return result.ToString();
+
+        }
+    }
+}
---
+++ ServerSide/Mails/Templates.cs
@@ -0,0 +1,40 @@
+﻿using sib_api_v3_sdk.Model;
+
+namespace ServerApi.Mails
+{
+    public class Templates
+    {
+        public static string SignInTemplate(string code)
+        {
+            return $@"
+                    <html>
+                        <head>
+                            <title>Welcome to Our Community</title>
+                        </head>
+                        <body>
+                            <h2>Hello from Spark!</h2>
+                            <p>Congratulations on creating your profile with us. We are thrilled to have you as a member of our community.</p>
+                            <p style='font-size: 24px; line-height: 36px;'>Your unique verification code is: <strong>{code:D6}</strong></p>
+                            <p>Thank you for choosing us. We look forward to seeing you around!</p>
+                        </body>
+                    </html>";
+        }
+
+        public static string RecoverPasswordTemplate(string email)
+        {
+            return $@"
+                    <html>
+                        <head>
+                            <title>Password recovery</title>
+                        </head>
+                        <body>
+                            <h2>Hello from Spark!</h2>
+                            <p>We received a request to reset your password. To proceed, click the link below:</p>
+                            <p><a href='{"https://localhost:44310/PasswordRecovery/?email=" + email}'>Reset Password</a></p>
+                            <p>If you didn't request a password reset, you can ignore this email.</p>
+                            <p>Thank you!</p>
+                        </body>
+                    </html>";
+        }
+    }
+}
